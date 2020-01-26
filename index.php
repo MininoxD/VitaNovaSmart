157fc0a815df46a495c15d356ed0132d76cff6f6
MininoxD <60321521+MininoxD@users.noreply.github.com>
2020-01-26 11:24:44 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,164 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/yeti/bootstrap.min.css" rel="stylesheet" integrity="sha384-bWm7zrSUE5E+21rA9qdH5frkMpXvqjQm/WJw4L5PYQLNHrywI5zs5saEjIcCdGu1" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Quattrocento+Sans:400i&display=swap" rel="stylesheet">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <!--hedaer-->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-image: url(img/fondoh.jpg);">
+            <a id="logo" href=""></a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="navbar-nav ml-auto">
+                    <a id="fb" class="redes" href="https://www.facebook.com/vita.novasmart?_rdc=1&_rdr"></a>
+                    <a id="tw" class="redes" href="#"></a>
+                    <a id="wts" class="redes" href="https://api.whatsapp.com/send?phone= +51 993106273"></a>
+                    <a id="li"  class="redes" href="https://web.facebook.com/vita.novasmart"></a>
+                    <a class="nav-item nav-link" href="#"></a>
+                </div>
+            </div>
+        </nav>
+        <!--
+        <nav class="menu dere">
+            <div class="lista dere">
+                <a href="#" class="item">asdgasdg</a>
+                <a href="#" class="item">asdgasdg</a>
+                <a href="#" class="item">asdgasdg</a>
+                <a href="#" class="item">asdgasdg</a>
+            </div>
+        </nav>
+        -->
+        <nav class="navbar navbar-light" style="padding:0px; background-color: #43bed4;">
+            <div class="lista dere ml-auto">
+                
+                <a href="#pro" class="item"><div class="icon"></div>PRODUCTOS</a>
+                <a href="#nosotros" class="item"><div class="icon"></div>NOSOTROS</a>   
+                <a href="#objetivos" class="item"><div class="icon"></div>OBJETIVOS</a>
+                <a href="#hubi" class="item"><div class="icon"></div>CONTACTENOS</a>
+                
+            </div>
+          </nav>     
+    </header>
+
+    <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-interval="10000">
+            <img src="img/slider/1.jpg" class="d-block w-100" alt="first slide">
+          </div>
+          <div class="carousel-item" data-interval="2000">
+            <img src="img/slider/2.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/slider/3.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/slider/4.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/slider/5.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/slider/6.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/slider/7.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      <!--background-image: url(img/fobj3.png); background-size: 100%-->
+      <div id="objetivos">
+          <nav class= "navbar navbar-light titulo" style="background-color:white;">
+            <h3>O B J E T I V O S</h3>
+            <img src="img/objetivos.png" alt="" class="obj">
+          </nav>
+      </div>
+
+      <nav class="navbar navbar-light" style="background-color:white; background-image: url(img/corfon2.jpg); background-repeat: no-repeat; padding: 0%; margin:0%;">
+        <img src="img/fcor.png" alt="" class="obj2">
+      </nav>
+
+
+      <div id="nosotros">
+          <nav class="navbar navbar-light bg-light titulo">
+            <h3>N O S O T R O S</h3>
+            <div class="almacen">
+              <h4>PRADO DE LA CRUZ FAUSTO JORGE</h4>
+              <p>Conformo el equipo de lucha contra el plomo en Pasco</p>
+              <p>ROL: líder y desarrollador del área biotecnológico de la STARTUP. Actualmente estudiante del 5to año de Medicina Humana sede Hospital Dos de Mayo, integre el equipo de plomo en sangre en Pasco.</p>
+            <img src="img/nosotros/2.png" alt="">
+            </div>
+
+            <div class="almacen2">
+              <h4>DE LA CRUZ PORRAS CRISTIAN</h4>
+                  <p>Estudiante de ingeniería metalurgia</p>
+                  <p>ROL: Identificador de las fuentes de los manantiales y encargado del área logística transporte y movilidad.</p>
+                <img src="img/nosotros/3.png" alt="">
+           </nav>
+
+          <nav class="navbar navbar-light bg-light">
+            <div class="almacen">
+              <h4>PRADO DE LA CRUZ LAURA</h4>
+                  <p>Organizó el I Congreso regional de ingeniería de sistemas orientado al futuro de la tecnología: CORISOFT</p>
+                  <p>ROL: Desarrolladora web y del aplicativo nacional 2019, bachiller en ingeniería en Sistemas y Computación.</p>
+                <img src="img/nosotros/1.png" alt="">
+            </div>
+
+            <div class="almacen2">
+              <h4>RODOLFO DE LA CRUZ CARLOS</h4>
+              <p>Constituyó la formación de la comunidad nuevo progreso en defensa de los comuneros y la biodiversidad de Pasco</p>
+              <p>ROL: Identificador de las fuentes naturales de manantiales 100 puros en la Región.</p>
+            <img src="img/nosotros/4.png" alt="">
+          </nav>
+      </div>
+
+      <div id="pro">
+        <nav class= "navbar navbar-light titulo  titulo" style="background-color: #3b89c5;">
+          <h3>P R O D U C T O</h3>
+          <div class="productos">
+              <h4>AGUA VITA NOVA</h4></br>
+              <p>"Agua pura de manantial rica en minerales y con propiedades energeticas  y alcalina con excelentes beneficios probioticos que potenciaran tu sistema inmune".</p>
+              <img src="img/producto/agua.jpg" alt="">
+          </div>
+        </nav>
+      </div>
+
+      <div id="hubi">
+        <nav class="navbar navbar-light bg-light titulo">
+          <h3>U B I C A C I O N</h3>
+          <div class="mapa">
+              <h4>NUESTRA HUBICACION</h4>
+              <p>Nos hubicamos en Av. Los Nogales 250 del distrito de Santa Anita, la referencia para llegar es</p>
+              <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8006178.857274053!2d-79.04144676787054!3d-11.532693211692358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c66ec981757f%3A0xdd59b1fb4505c6bd!2sAv.%20Los%20Nogales%20350%2C%20Santa%20Anita%2015007!5e0!3m2!1ses-419!2spe!4v1579891520390!5m2!1ses-419!2spe" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe></div>
+          </div>
+        </nav>
+      </div>
+
+      <footer>
+          <nav class="navbar navbar-light" style="background-color: #43bed4;">
+            <p style="text-align: center;"></p>© Copyright 2020 </p>
+          </nav>
+      </footer>
+    <!--hedaer-->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
